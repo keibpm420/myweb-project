82cea653dec499be4db92a74a8945a520a962766
keibpm420 <keibpm420@gmail.com>
2025-12-18 16:31:22 +0900

    vercel用の設定変更

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,18 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Top from "../pages/Top";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Entry from "../pages/Entry";
-
-const basename = import.meta.env.MODE === "production" ? "/project" : "/";
 
 const Router = () => {
+    const basename = "/";
+
     return (
         <BrowserRouter basename={basename}>
             <Header />
 
             <Routes>
                 <Route path="/" element={<Top />} />
-                <Route path="/entry" element={<Entry />} />
             </Routes>
 
             <Footer />
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/", // ← ここ重要。通常は '/' でOK
+    base: "/",
 });
