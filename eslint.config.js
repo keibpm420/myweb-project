b8ccc12fe9c2570bfbec6459d3c08aafb9a6720f
keibpm420 <keibpm420@gmail.com>
2025-12-31 14:56:53 +0900

    設定型を変更

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,42 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
-    globalIgnores(['dist']),
+export default tseslint.config(
+    // 1. 基本となる設定をそのまま並べる
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    // 2. 個別の詳細設定
     {
-        files: ['**/*.{js,jsx}'],
-        extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+        ignores: ["dist"],
+    },
+    {
+        files: ["**/*.{ts,tsx,js,jsx}"],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
             parserOptions: {
-                ecmaVersion: 'latest',
-                ecmaFeatures: { jsx: true },
-                sourceType: 'module',
+                parser: tseslint.parser,
             },
         },
-        rules: {
-            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
         },
-    },
-]);
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { varsIgnorePattern: "^[A-Z_]" },
+            ],
+        },
+    }
+);
