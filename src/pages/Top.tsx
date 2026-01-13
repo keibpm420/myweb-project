2cbd75c161cdb1fe4b6f8b7c4fd02adbc4e52d2b
keibpm420 <keibpm420@gmail.com>
2026-01-14 00:56:14 +0900

    Add files via upload

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -6,6 +6,7 @@ import Flow from "../components/Top/Flow";
 import Faq from "../components/Top/Faq";
 import Interview from "../components/Top/Interview";
 import ScrollAnimations from "../components/animateAllTitles";
+import { Link } from "react-router-dom";
 
 const Top = () => {
     return (
@@ -24,6 +25,20 @@ const Top = () => {
             <Interview />
 
             <Faq />
+            <div className="top-followBtn">
+                <ul>
+                    <li>
+                        <Link className="a" to="/reservations">
+                            <span>予約する</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/job">
+                            <span>求人を見る</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </main>
     );
 };
